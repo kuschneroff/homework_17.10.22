bea28eb6ef12d766cd3c1c2ba74e70e530808682
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-16 15:15:03 +0300

    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ Task_#34/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+Console.WriteLine("Сгенерированный массив из случайных положительных трёхзначных чисел");
+
+int [] RandomArray()
+{
+    int [] array = new int [10];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+        //if(array[i] >= 99 && array[i] <= 1000);
+    }
+    return array;
+}
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int AnEvenNumber(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        {            
+            if(array[i] % 2 == 0)
+            count += 1;
+        }
+    return count;
+}
+int [] arr = RandomArray();
+PrintArray(arr);
+Console.WriteLine();
+int countAnEvenNumber = AnEvenNumber(arr);
+Console.WriteLine($"Количество чётных чисел в массиве = {countAnEvenNumber}");
